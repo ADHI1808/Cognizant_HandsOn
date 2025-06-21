8ed88c5a0917fa9b6efb218c5eaadaf88014e4a8
Adhithyan S <s.adhithyan2004@gmail.com>
2025-06-22 02:22:24 +0530

    Create Financial_Forecasting.cs

---
+++ week1/Financial_Forecasting.cs
@@ -0,0 +1,22 @@
+using System;
+class FinancialForecastingWithInput
+{
+    static void Main()
+    {
+        Console.Write("Enter Present Value (₹): ");
+        double presentValue = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter Annual Growth Rate (%): ");
+        double growthRatePercent = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter Number of Years: ");
+        int years = Convert.ToInt32(Console.ReadLine());
+        double growthRate = growthRatePercent / 100;
+        double futureValue = CalculateFutureValue(presentValue, growthRate, years);
+        Console.WriteLine("Future Value after " + years + " years: ₹" + futureValue.ToString("F2"));
+    }
+    static double CalculateFutureValue(double presentValue, double growthRate, int years)
+    {
+        if (years == 0)
+            return presentValue;
+        return (1 + growthRate) * CalculateFutureValue(presentValue, growthRate, years - 1);
+    }
+}
